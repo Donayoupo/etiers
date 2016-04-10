fd938a12b5f3c8a926ffac44e2d7132e76c203a0
donayoupo <shacqk@hotmail.com>
2016-04-10 07:51:58 +0200

    second commoit

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+	String valeur = "value";
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
